32d751547eee282baac661ff89a846441fbd81be
Mayara Mafioletti <mayaracardinot@hotmail.com>
2021-02-19 10:39:23 -0300

    Adicionando o Spring initialezr

---
+++ src/test/java/br/com/raizesDela/raizesDela/RaizesDelaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raizesDela.raizesDela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaizesDelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
